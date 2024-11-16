8bd66ae8337c5da0f3c26be9cf7bc7fb6182d2a1
Hemant Vetal <hemantvetal813@gmail.com>
2024-11-16 20:59:02 +0530

    first commit

---
+++ app/room/[roomId]/page.tsx
@@ -0,0 +1,162 @@
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/compat/router";
+import React, { useEffect, useRef, useState } from "react";
+import { io, Socket } from "socket.io-client";
+import { useParams } from "next/navigation";
+const socket = io('ws://localhost:3001/local', {
+  autoConnect: false,
+  auth: {
+    // token: userData?.token,
+    userId: 1,
+  }
+});
+socket.connect();
+
+export default function Room() {
+
+  // const router = useRouter();
+  const { roomId } = useParams();
+  socket.emit("join_room", roomId);
+
+  const [inputValue, setInputValue] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const handleSendMessage = () => {
+    if (inputValue.trim() === "") return;
+
+    setMessages([...messages, { room: roomId, text: inputValue, sender: "User" }]);
+    setInputValue("");
+
+    socket.emit("send_message", { room: roomId, text: inputValue });
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") handleSendMessage();
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      console.log(data.text);
+      setMessages([...messages, data]);
+    });
+  }, [socket, messages]);
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          { messages.map((msg, i) => {
+            return (
+              <li key={ i }>{ msg.text }</li>
+            );
+          }) }
+        </ol>
+        <input
+          type="text"
+          value={ inputValue }
+          onChange={ handleInputChange }
+          onKeyDown={ handleKeyPress }
+          placeholder=" Type a message..."
+        />
+        <button onClick={ handleSendMessage }>Send</button>
+        {/* <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        /> */}
+        {/* <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+           <li className="mb-2">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+              app/page.tsx
+            </code>
+            .
+          </li>
+          <li>Save and see your changes instantly.</li>
+
+        </ol> */}
+
+        {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
+        </div> */}
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        {/* <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a> */}
+      </footer>
+    </div>
+  );
+}
